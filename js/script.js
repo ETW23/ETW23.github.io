80673d0e21a16bf444ce2ef81b565d42386124c1
ETW23 <145110013+ETW23@users.noreply.github.com>
2024-04-03 12:35:15 +0200

    hvg
    ghg

--- js/script.js
+++ js/script.js
@@ -1,85 +1,81 @@
-
-
 ///////////// MENU ///////////////////
 document.getElementById("menunav").style.right = "-20em";
 document
-    .getElementById("botonHamburguesa")
-    .addEventListener("click", function () {
-        var menu = document.getElementById("menunav");
-        if (menu.style.right == "-20em") {
-            menu.style.right = "0em";
-            menu.style.transition = "1s";
-        } else {
-            menu.style.right = "-20em";
-        }
-    });
+  .getElementById("botonHamburguesa")
+  .addEventListener("click", function () {
+    var menu = document.getElementById("menunav");
+    if (menu.style.right == "-20em") {
+      menu.style.right = "0em";
+      menu.style.transition = "1s";
+    } else {
+      menu.style.right = "-20em";
+    }
+  });
 
-var tarjetas = document.getElementsByClassName('box')
+var tarjetas = document.getElementsByClassName("box");
 for (let i = 0; i < tarjetas.length; i++) {
-    tarjetas[i].onmouseover = function () {
-        this.style.filter = 'opacity(100%)';
-    }
-    tarjetas[i].onmouseout = function () {
-        this.style.filter = 'opacity(60%)'
-    }
+  tarjetas[i].onmouseover = function () {
+    this.style.filter = "opacity(100%)";
+  };
+  tarjetas[i].onmouseout = function () {
+    this.style.filter = "opacity(60%)";
+  };
 }
 
 // animación carrousel header
 
 var images = [
-    "images/DSC_9832.JPG",
-    "images/portada2.jpg",
-    "modulo/images/DSC_9856.JPG"
+  "images/DSC_9832.JPG",
+  "images/portada2.jpg",
+  "modulo/images/DSC_9856.JPG",
 ];
 
 function changeBackgroundImage() {
-    var backgroundContainer = document.getElementById("background-container");
-    var currentImageIndex = 0;
+  var backgroundContainer = document.getElementById("background-container");
+  var currentImageIndex = 0;
 
-    setInterval(function () {
-        currentImageIndex = (currentImageIndex + 1) % images.length;
-        backgroundContainer.classList.add("transition-background");
-        backgroundContainer.style.backgroundImage = "url(" + images[currentImageIndex] + ")";
-        setTimeout(function () {
-            backgroundContainer.classList.remove("transition-background");
-        }, 500); // Duración de la transición en milisegundos
-    }, 3000);
+  setInterval(function () {
+    currentImageIndex = (currentImageIndex + 1) % images.length;
+    backgroundContainer.classList.add("transition-background");
+    backgroundContainer.style.backgroundImage =
+      "url(" + images[currentImageIndex] + ")";
+    setTimeout(function () {
+      backgroundContainer.classList.remove("transition-background");
+    }, 500); // Duración de la transición en milisegundos
+  }, 3000);
 }
 
 changeBackgroundImage();
 
 /////////////////Click derecho desactivado///////////////////
 
-for (let event of ['cut', 'copy', 'paste', 'contextmenu']) {
-    window.addEventListener(event, e => e.preventDefault())
+for (let event of ["cut", "copy", "paste", "contextmenu"]) {
+  window.addEventListener(event, (e) => e.preventDefault());
 }
-///////////////////////////////////////////////////////////    
-var popupContainer = document.getElementById('popup-container');
-var closeButton = document.getElementById('close-btn');
+///////////////////////////////////////////////////////////
+var popupContainer = document.getElementById("popup-container");
+var closeButton = document.getElementById("close-btn");
 
-window.addEventListener('DOMContentLoaded', function () {
-
-    if (!localStorage.getItem('popupShown')) {
-        popupContainer.style.display = 'block';
-    }
-    closeButton.addEventListener('click', function () {
-        popupContainer.style.display = 'none';
-        localStorage.setItem('popupShown', true);
-    });
+window.addEventListener("DOMContentLoaded", function () {
+  if (!localStorage.getItem("popupShown")) {
+    popupContainer.style.display = "block";
+  }
+  closeButton.addEventListener("click", function () {
+    popupContainer.style.display = "none";
+    localStorage.setItem("popupShown", true);
+  });
 });
 
+var mostrar = document.getElementById("mostrarNota");
 
-var mostrar = document.getElementById('mostrarNota');
-
-mostrar.addEventListener('click', function () {
-    popupContainer.style.display = 'block';
-    localStorage.setItem('popupShown', true);
+mostrar.addEventListener("click", function () {
+  popupContainer.style.display = "block";
+  localStorage.setItem("popupShown", true);
 });
 ////////////////////////////////////////////////
 
 ////////////////////////////////////////////
 
 botonNotis.onclick = function () {
-    open('noticias/', "_self")
-}
-
+  open("noticias/", "_self");
+};
